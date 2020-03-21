f9dbc1ad15e7f812aca22a078f1e47aa9ce36aef
GeometricStoat <50852017+GeometricStoat@users.noreply.github.com>
2020-03-21 00:04:10 -0500

    Implemented lazy loading

--- src/router.js
+++ src/router.js
@@ -1,13 +1,9 @@
 import Home from "@/views/Home";
-import Credits from "@/views/Credits";
-import Submit from "@/views/Submit";
-import Details from "@/views/Details";
-// TODO: lazy/split loading
 
 export default [
     {path: "/", name: "home", component: Home},
-    {path: "/credits", name: "credits", component: Credits},
-    {path: "/submit", name: "submit", component: Submit},
-    {path: "/details/:id", name: "details", component: Details}
+    {path: "/credits", name: "credits", component: import("@/views/Credits")},
+    {path: "/submit", name: "submit", component: import("@/views/Submit")},
+    {path: "/details/:id", name: "details", component: import("@/views/Details")}
     // TODO: add report page route
 ];
