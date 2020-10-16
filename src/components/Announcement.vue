94693ebc4611f636ab602c93cdc8f281090bd469
Amrit Rathie <amrit.rathie@gmail.com>
2020-10-16 12:24:54 -0500

    Add announcement banner

---
+++ src/components/Announcement.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+			<p class="short" @click="showDesc = !showDesc"><strong>{{ short }}</strong></p>
+				<p v-show="showDesc" class="long" aria-hidden="false">{{ long }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Announcement",
+				props: ["short", "long"],
+				data() {
+					return {
+						showDesc: false
+					}
+				}
+    }
+</script>
+
+<style scoped>
+    div {
+				background-color: whitesmoke;
+				padding: 1em 0.5em;
+    }
+</style>
