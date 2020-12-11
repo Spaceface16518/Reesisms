df36088ec9e9af5ffc22e48b7678ac7e8c119e16
Amrit <amrit.rathie@gmail.com>
2020-12-11 10:01:53 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
 				<aside>
-					<Announcement short="Reesisms is getting a major update!" long="Reesisms will be moving to reesisms.com, getting a snazzy new look, and a lot of internal changes. Stay tuned!" />
+					<Announcement short="Reesisms is getting a major update!" long="Reesisms will be getting a new look and a lot of internal changes. Stay tuned!" />
 				</aside>
         <nav>
             <router-link to="/">Home</router-link>
